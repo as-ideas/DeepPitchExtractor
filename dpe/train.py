Fix normalization.

dpe/train.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
                     'config': config,
                     'step': step}, cp_path / 'latest_model.pt')
 
-        val_loss, val_batch, logits = 0, None, None
+        val_loss, val_batch, logits, pitch_target = 0, None, None, None
         for val_batch in val_batches:
             pitch_target = normalize_pitch(val_batch['pitch'].to(device),
                                            pmin=pmin, pmax=pmax, n_channels=out_channels).to(device)
@@ -92,8 +92,6 @@ if __name__ == '__main__':
             val_loss += loss
 
         spec_len = val_batch['spec_len'][0]
-        pitch_target = normalize_pitch(val_batch['pitch'],
-                                       pmin=pmin, pmax=pmax, n_channels=out_channels)
         pitch_pred = torch.argmax(logits, dim=1)
         pitch_pred_nonzeros = torch.argmax(logits[:, 1:, :], dim=1) + 1
         pitch_target_fig = plot_pitch(pitch_target[0, :spec_len].cpu().numpy())